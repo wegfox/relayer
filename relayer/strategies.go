f4d57bcbb383c20cc6cda3b216ec87c0cbde8e9c
Jack Zampolin <jack.zampolin@gmail.com>
2021-09-13 18:28:27 -0700

    Better logging

--- relayer/strategies.go
+++ relayer/strategies.go
@@ -47,41 +47,48 @@ type StrategyCfg struct {
 // RunStrategy runs a given strategy
 func RunStrategy(src, dst *Chain, strategy Strategy) (func(), error) {
 	doneChan := make(chan struct{})
-	// Keep trying until we're timed out or got a result or got an error
 	go func() {
 		for {
 			select {
-			// Got a timeout! fail with a timeout error
 			case <-doneChan:
 				return
 			default:
-				fmt.Println("tick begin")
 				// Fetch any unrelayed sequences depending on the channel order
 				sp, err := strategy.UnrelayedSequences(src, dst)
 				if err != nil {
 					src.Log(fmt.Sprintf("unrelayed sequences error: %s", err))
 				}
-
-				fmt.Println("unrelayed sequences passed")
-				if err = strategy.RelayPackets(src, dst, sp); err != nil {
-					src.Log(fmt.Sprintf("relay packets error: %s", err))
+				if len(sp.Src) > 0 {
+					src.Log(fmt.Sprintf("[%s] unrelayed-packets-> %v", src.ChainID, sp.Src))
+				}
+				if len(sp.Dst) > 0 {
+					dst.Log(fmt.Sprintf("[%s] unrelayed-packets-> %v", dst.ChainID, sp.Dst))
+				}
+				if !sp.Empty() {
+					if err = strategy.RelayPackets(src, dst, sp); err != nil {
+						src.Log(fmt.Sprintf("relay packets error: %s", err))
+					}
 				}
 
-				fmt.Println("relay packets passed")
+				// Fetch any unrelayed acks depending on the channel order
 				ap, err := strategy.UnrelayedAcknowledgements(src, dst)
 				if err != nil {
 					src.Log(fmt.Sprintf("unrelayed acks error: %s", err))
 				}
-
-				fmt.Println("relay acks passed")
-				if err = strategy.RelayAcknowledgements(src, dst, ap); err != nil {
-					src.Log(fmt.Sprintf("relay acks error: %s", err))
+				if len(ap.Src) > 0 {
+					src.Log(fmt.Sprintf("[%s] unrelayed-acks-> %v", src.ChainID, ap.Src))
+				}
+				if len(ap.Dst) > 0 {
+					dst.Log(fmt.Sprintf("[%s] unrelayed-acks-> %v", dst.ChainID, ap.Dst))
+				}
+				if !ap.Empty() {
+					if err = strategy.RelayAcknowledgements(src, dst, ap); err != nil {
+						src.Log(fmt.Sprintf("relay acks error: %s", err))
+					}
 				}
 				time.Sleep(100 * time.Millisecond)
 			}
 		}
 	}()
-
-	// Return a function to stop the relayer goroutine
 	return func() { doneChan <- struct{}{} }, nil
 }
