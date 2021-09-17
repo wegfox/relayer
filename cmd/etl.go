dc9a8990aa6bee69aad022d52a0aca1a97db93d1
Jack Zampolin <jack.zampolin@gmail.com>
2021-09-17 14:40:18 -0700

    Push etl work

--- cmd/etl.go
+++ cmd/etl.go
@@ -27,6 +27,7 @@ import (
 	channeltypes "github.com/cosmos/ibc-go/modules/core/04-channel/types"
 	"github.com/cosmos/relayer/relayer"
 	"github.com/spf13/cobra"
+	"golang.org/x/sync/errgroup"
 )
 
 // queryCmd represents the chain command
@@ -63,7 +64,7 @@ func qosCmd() *cobra.Command {
 		Use:     "quality-of-servce [path] [src_start] [dst_start]",
 		Aliases: []string{"qos"},
 		Short:   "query denomination traces for a given network by chain ID",
-		Args:    cobra.ExactArgs(1),
+		Args:    cobra.ExactArgs(3),
 		Example: strings.TrimSpace(fmt.Sprintf(`
 $ %s query ibc-denoms ibc-0
 $ %s q ibc-denoms ibc-0`,
@@ -74,79 +75,93 @@ $ %s q ibc-denoms ibc-0`,
 			if err != nil {
 				return err
 			}
-
-			srcStart, err := strconv.ParseInt(args[0], 10, 64)
+			srcStart, err := strconv.ParseInt(args[1], 10, 64)
 			if err != nil {
 				return err
 			}
-			dstStart, err := strconv.ParseInt(args[1], 10, 64)
+			dstStart, err := strconv.ParseInt(args[2], 10, 64)
 			if err != nil {
 				return err
 			}
-			srcBlocks := []int64{srcStart}
-			dstBlocks := []int64{dstStart}
-
-			srcCurrent, dstCurrent, err := relayer.QueryLatestHeights(c[src], c[dst])
+			srcBlocks, dstBlocks, err := makeBlockArrays(c[src], c[dst], srcStart, dstStart)
 			if err != nil {
+				return nil
+			}
+			fmt.Printf("chain-id[%s] startBlock(%d) endBlock(%d)\n", src, srcBlocks[0], srcBlocks[len(srcBlocks)-1])
+			fmt.Printf("chain-id[%s] startBlock(%d) endBlock(%d)\n", dst, dstBlocks[0], dstBlocks[len(dstBlocks)-1])
+			if err := QueryBlocks(c[src], srcBlocks); err != nil {
 				return err
 			}
-
-			for i := srcStart; i < srcCurrent; i++ {
-				srcBlocks = append(srcBlocks, i)
-			}
-
-			for i := dstStart; i < dstCurrent; i++ {
-				dstBlocks = append(dstBlocks, i)
-			}
-
-			for _, h := range srcBlocks {
-				block, err := c[src].Client.Block(context.Background(), &h)
-				if err != nil {
-					return err
-				}
-				for _, tx := range block.Block.Data.Txs {
-					sdkTx, err := c[src].Encoding.TxConfig.TxDecoder()(tx)
-					if err != nil {
-						return err
-					}
-					for _, msg := range sdkTx.GetMsgs() {
-						handleMsg(msg, block.Block.Height, block.Block.Time, src)
-					}
-				}
-			}
-
-			for _, h := range dstBlocks {
-				block, err := c[dst].Client.Block(context.Background(), &h)
-				if err != nil {
-					return err
-				}
-				for _, tx := range block.Block.Data.Txs {
-					sdkTx, err := c[dst].Encoding.TxConfig.TxDecoder()(tx)
-					if err != nil {
-						return err
-					}
-					for _, msg := range sdkTx.GetMsgs() {
-						handleMsg(msg, block.Block.Height, block.Block.Time, dst)
-					}
-				}
-			}
-
-			return nil
+			return QueryBlocks(c[dst], dstBlocks)
 		},
 	}
 
 	return cmd
 }
 
-func handleMsg(msg sdk.Msg, height int64, timestamp time.Time, chainid string) {
+func QueryBlocks(chain *relayer.Chain, blocks []int64) error {
+	fmt.Println("starting block queries for", chain.ChainID)
+	var eg errgroup.Group
+	sem := make(chan struct{}, 1000)
+	for _, h := range blocks {
+		h := h
+		sem <- struct{}{}
+		eg.Go(func() error {
+			block, err := chain.Client.Block(context.Background(), &h)
+			if err != nil {
+				return err
+			}
+			for _, tx := range block.Block.Data.Txs {
+				sdkTx, err := chain.Encoding.TxConfig.TxDecoder()(tx)
+				if err != nil {
+					return err
+				}
+				for _, msg := range sdkTx.GetMsgs() {
+					handleMsg(chain, msg, block.Block.Height, block.Block.Time, chain.ChainID)
+				}
+			}
+			<-sem
+			return nil
+		})
+	}
+	return eg.Wait()
+	// return nil
+}
+
+func makeBlockArrays(src, dst *relayer.Chain, srcStart, dstStart int64) ([]int64, []int64, error) {
+	srcBlocks := []int64{}
+	dstBlocks := []int64{}
+	srcCurrent, dstCurrent, err := relayer.QueryLatestHeights(src, dst)
+	if err != nil {
+		return srcBlocks, dstBlocks, err
+	}
+	for i := srcStart; i < srcCurrent; i++ {
+		srcBlocks = append(srcBlocks, i)
+	}
+	for i := dstStart; i < dstCurrent; i++ {
+		dstBlocks = append(dstBlocks, i)
+	}
+	return srcBlocks, dstBlocks, nil
+}
+
+func handleMsg(c *relayer.Chain, msg sdk.Msg, height int64, timestamp time.Time, chainid string) {
 	switch m := msg.(type) {
 	case *transfertypes.MsgTransfer:
-		fmt.Printf("%s => [%s]@{%d} *transfertypes.MsgTransfer [%x]", timestamp.String(), chainid, height, m.GetSigners()[0].Bytes())
+		done := c.UseSDKContext()
+		fmt.Printf("%s => [%s]@{%d} *transfertypes.MsgTransfer [%x]\n", timestamp.String(), chainid, height, m.GetSigners()[0].Bytes())
+		done()
 	case *channeltypes.MsgRecvPacket:
-		fmt.Printf("%s => [%s]@{%d} *channeltypes.MsgRecvPacket [%x]", timestamp.String(), chainid, height, m.GetSigners()[0].Bytes())
+		done := c.UseSDKContext()
+		fmt.Printf("%s => [%s]@{%d} *channeltypes.MsgRecvPacket [%x]\n", timestamp.String(), chainid, height, m.GetSigners()[0].Bytes())
+		done()
 	case *channeltypes.MsgTimeout:
-		fmt.Printf("%s => [%s]@{%d} *channeltypes.MsgTimeout [%x]", timestamp.String(), chainid, height, m.GetSigners()[0].Bytes())
+		done := c.UseSDKContext()
+		fmt.Printf("%s => [%s]@{%d} *channeltypes.MsgTimeout [%x]\n", timestamp.String(), chainid, height, m.GetSigners()[0].Bytes())
+		done()
 	case *channeltypes.MsgAcknowledgement:
-		fmt.Printf("%s => [%s]@{%d} *channeltypes.MsgAcknowledgement [%x]", timestamp.String(), chainid, height, m.GetSigners()[0].Bytes())
+		done := c.UseSDKContext()
+		fmt.Printf("%s => [%s]@{%d} *channeltypes.MsgAcknowledgement [%x]\n", timestamp.String(), chainid, height, m.GetSigners()[0].Bytes())
+		done()
+	default:
 	}
 }
